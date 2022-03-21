b388797297e4388b0c129a2e846ece1ed92ea7b9
karthiknambiar29 <karthiknambiar4u@gmail.com>
2022-03-22 00:07:01 +0530

    cards and decks

---
+++ static/js/components/category.js
@@ -0,0 +1,45 @@
+var category = {
+    data: function() {
+        return {
+            title: "",
+            description: ""
+        }
+    },
+    template: `
+        <div>
+            <h1>FlashCards</h1>
+            <h1>{{ title }}</h1>
+            <h2>{{ description }}</h2>
+            <b-button class="submit-button" @click="startQuiz" variant="outline-primary">Start Quiz</b-button>
+        </div>
+    `,
+    methods: {
+        async getallCard() {
+            try {
+                const response = await fetch(`http://172.28.134.31:8080/api/allcards/${this.$route.params.category_id}`, {
+                    method : 'GET',
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${localStorage.getItem('jwt-token')}`,
+                    }
+                })
+                const data = await response.json();
+                this.data = data
+                localStorage.setItem("card_ids", data.card_ids)
+                this.description = data.description;
+                this.title = data.title;
+                console.log(data)
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        startQuiz() {
+            this.$router.push({path:`/quiz/${this.$route.params.category_id}/1`})
+        }
+    },
+    mounted() {
+        this.getallCard();
+    }
+};
+
+export default category;
---
+++ static/js/components/quiz.js
@@ -0,0 +1,74 @@
+var quiz = {
+    data: function() {
+        return {
+            additional: [],
+            additional_grouped: [],
+            options: [],
+            title: "",
+        }
+    },
+    template: `
+        <div>
+            <b-card :title="title" border-variant="primary"
+                header-bg-variant="primary"
+                header-text-variant="white"
+                align="center">
+                <b-form-group>
+                    <b-form-radio-group
+                        id="checkbox"
+                        v-model="additional_grouped"
+                        :options="options"></b-form-radio-group>
+                </b-form-group>
+            </b-card>
+            <b-button class="submit-button" @click="nextQuestion" variant="outline-primary">Next</b-button></b-link>
+        </div>
+    `,
+    methods: {
+        async getCard() {
+            try {
+                var card_id = localStorage.getItem("card_ids").split(",")[parseInt(this.$route.params.question)-1]
+                console.log(typeof(localStorage.getItem("card_ids")))
+                const response = await fetch(`http://172.28.134.31:8080/api/card/${card_id}`, {
+                    method : 'GET',
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${localStorage.getItem('jwt-token')}`,
+                    }
+                })
+                const data = await response.json();
+                console.log(data)
+                // console.log(this.$route.params.id)
+                this.title = data.front;
+                this.options = []
+                this.options.push({text: data.option_1, value: "1"})
+                this.options.push({text: data.option_2, value: "2"})
+                this.options.push({text: data.option_3, value: "3"})
+                this.options.push({text: data.option_4, value: "4"})
+                
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        nextQuestion() {
+            // var answers = localStorage.getItem("answers");
+            // if (!answers) {
+            //     const ans = JSON.stringify({$route.params.question): this.additional_grouped})
+            // }
+            if (parseInt(this.$route.params.question) < 10) {
+                this.$router.push({path: `/quiz/${this.$route.params.category_id}/${parseInt(this.$route.params.question)+1}`})
+            } else {
+                this.$router.push({path: "/score"})
+            }
+        }
+    },
+    mounted() {
+        this.getCard();
+    },
+    watch: {
+        "$route.params.question": function(to, from) {
+            this.getCard()
+        }
+    }
+};
+
+export default quiz;
